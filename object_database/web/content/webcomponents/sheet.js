72b93e0d23becca5b822496ffbf40bd9140f48a2
dkrasner <krasnerdaniel@gmail.com>
2019-09-06 13:27:57 -0400

    saving last web comp sheet version

--- object_database/web/content/webcomponents/sheet.js
+++ object_database/web/content/webcomponents/sheet.js
@@ -1,28 +1,38 @@
 class AprioriSheet extends HTMLElement {
+    // expose non-inherited (custom) attributes
     static get observedAttributes() {
-        return ['text'];
+        return ['rows', 'cols'];
     }
 
     constructor() {
         super();
         var shadow = this.attachShadow({mode: 'open'});
-        var wrapper = document.createElement('span');
-        shadow.appendChild(wrapper);
+        this.build(shadow)
         console.log("construction apriori sheet web component");
     }
 
+    // this happens after the component is registered but not fully inserted into the
+    // DOM by maquette
     connectedCallback() {
-        const shadow = this.shadowRoot;
-        console.log('getting sheet attribute: ' + this.getAttribute("text"));
-        shadow.querySelector('span').textContent = this.getAttribute("text");
-
+        console.log('initial document DOM append of sheet, id: ' + this.getAttribute('id'));
     }
 
+    // this is effectively our <<first>> load, since it happens after maquette inserts the
+    // sheet into the DOM
     attributeChangedCallback() {
         const shadow = this.shadowRoot;
-        console.log('getting sheet attribute: ' + this.getAttribute("text"));
-        shadow.querySelector('span').textContent = this.getAttribute("text");
-
+        let table = shadow.querySelector('table');
     }
+
+    // our element is disconnected from the document's DOM
+    disconnectedCallback() {
+        console.log('disconnecting of sheet, id: ' + this.getAttribute('id'));
+    }
+
+    build(shadow) {
+        var wrapper = document.createElement('table');
+        shadow.appendChild(wrapper);
+    }
+
 }
 customElements.define('apriori-sheet', AprioriSheet);

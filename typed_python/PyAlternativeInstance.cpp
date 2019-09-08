01de019306d9f29efb8140013dfd7b8765652f24
Braxton Mckee <braxton@aprioriinvestments.com>
2019-09-08 12:52:46 -0400

    Fix a bug in type-conversion to ListOf/TupleOf. Refs #219.
    Type-conversion is supposed to proceed in two passes - first, with 'explicit' set to
    False, which is intended to not do any deep conversion, and then again with 'explicit'
    set to True, which allows a broader set of conversions. This is intended to allow us
    to have multiple overloads of function types and to get approximately correct behavior
    without forcing the very first specialization to work. Similarly, it allows
    OneOf(int, float) and OneOf(int, str) to both accept floats.
    
    We had two bugs here - first, we were not obeying the rules in List/Tuple conversion.
    Second, we were not clearing the exception bit when catching a 'PythonExceptionSet'
    exception, which leads to corruption of the python thread state.

---
+++ typed_python/PyAlternativeInstance.cpp
@@ -0,0 +1,369 @@
+/******************************************************************************
+   Copyright 2017-2019 Nativepython Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+******************************************************************************/
+
+#include "PyAlternativeInstance.hpp"
+#include "PyFunctionInstance.hpp"
+
+Alternative* PyAlternativeInstance::type() {
+    return (Alternative*)extractTypeFrom(((PyObject*)this)->ob_type);
+}
+
+ConcreteAlternative* PyConcreteAlternativeInstance::type() {
+    return (ConcreteAlternative*)extractTypeFrom(((PyObject*)this)->ob_type);
+}
+
+PyObject* PyAlternativeInstance::pyTernaryOperatorConcrete(PyObject* rhs, PyObject* thirdArg, const char* op, const char* opErr) {
+    auto it = type()->getMethods().find(op);
+
+    if (it != type()->getMethods().end()) {
+        Function* f = it->second;
+
+        PyObjectStealer argTuple(
+            PyTuple_Pack(3, (PyObject*)this, (PyObject*)rhs, (PyObject*)thirdArg)
+            );
+
+        std::pair<bool, PyObject*> res =
+            PyFunctionInstance::tryToCallAnyOverload(f, nullptr, argTuple, nullptr);
+        if (res.first) {
+            return res.second;
+        }
+    }
+
+    return ((PyInstance*)this)->pyTernaryOperatorConcrete(rhs, thirdArg, op, opErr);
+}
+PyObject* PyConcreteAlternativeInstance::pyTernaryOperatorConcrete(PyObject* rhs, PyObject* thirdArg, const char* op, const char* opErr) {
+    auto it = type()->getAlternative()->getMethods().find(op);
+
+    if (it != type()->getAlternative()->getMethods().end()) {
+        Function* f = it->second;
+
+        PyObjectStealer argTuple(
+            PyTuple_Pack(3, (PyObject*)this, (PyObject*)rhs, (PyObject*)thirdArg)
+            );
+        std::pair<bool, PyObject*> res =
+            PyFunctionInstance::tryToCallAnyOverload(f, nullptr, argTuple, nullptr);
+        if (res.first) {
+            return res.second;
+        }
+    }
+
+    return ((PyInstance*)this)->pyTernaryOperatorConcrete(rhs, thirdArg, op, opErr);
+}
+
+PyObject* PyAlternativeInstance::pyOperatorConcrete(PyObject* rhs, const char* op, const char* opErr) {
+    auto it = type()->getMethods().find(op);
+
+    if (it != type()->getMethods().end()) {
+        Function* f = it->second;
+
+        PyObjectStealer argTuple(
+            PyTuple_Pack(2, (PyObject*)this, (PyObject*)rhs)
+            );
+
+        std::pair<bool, PyObject*> res =
+            PyFunctionInstance::tryToCallAnyOverload(f, nullptr, argTuple, nullptr);
+        if (res.first) {
+            return res.second;
+        }
+    }
+
+    return ((PyInstance*)this)->pyOperatorConcrete(rhs, op, opErr);
+}
+PyObject* PyConcreteAlternativeInstance::pyOperatorConcrete(PyObject* rhs, const char* op, const char* opErr) {
+    auto it = type()->getAlternative()->getMethods().find(op);
+
+    if (it != type()->getAlternative()->getMethods().end()) {
+        Function* f = it->second;
+
+        PyObjectStealer argTuple(
+            PyTuple_Pack(2, (PyObject*)this, (PyObject*)rhs)
+            );
+
+        std::pair<bool, PyObject*> res =
+            PyFunctionInstance::tryToCallAnyOverload(f, nullptr, argTuple, nullptr);
+        if (res.first) {
+            return res.second;
+        }
+    }
+
+    return ((PyInstance*)this)->pyOperatorConcrete(rhs, op, opErr);
+}
+
+PyObject* PyAlternativeInstance::pyUnaryOperatorConcrete(const char* op, const char* opErr) {
+    auto it = type()->getMethods().find(op);
+
+    if (it != type()->getMethods().end()) {
+        Function* f = it->second;
+
+        PyObjectStealer argTuple(
+            PyTuple_Pack(2, (PyObject*)this)
+            );
+
+        std::pair<bool, PyObject*> res =
+            PyFunctionInstance::tryToCallAnyOverload(f, nullptr, argTuple, nullptr);
+        if (res.first) {
+            return res.second;
+        }
+    }
+
+    return ((PyInstance*)this)->pyUnaryOperatorConcrete(op, opErr);
+}
+PyObject* PyConcreteAlternativeInstance::pyUnaryOperatorConcrete(const char* op, const char* opErr) {
+    auto it = type()->getAlternative()->getMethods().find(op);
+
+    if (it != type()->getAlternative()->getMethods().end()) {
+        Function* f = it->second;
+
+        PyObjectStealer argTuple(
+            PyTuple_Pack(2, (PyObject*)this)
+            );
+
+        std::pair<bool, PyObject*> res =
+            PyFunctionInstance::tryToCallAnyOverload(f, nullptr, argTuple, nullptr);
+        if (res.first) {
+            return res.second;
+        }
+    }
+
+    return ((PyInstance*)this)->pyUnaryOperatorConcrete(op, opErr);
+}
+
+void PyConcreteAlternativeInstance::constructFromPythonArgumentsConcrete(ConcreteAlternative* alt, uint8_t* data, PyObject* args, PyObject* kwargs) {
+    alt->constructor(data, [&](instance_ptr p) {
+        if ((kwargs == nullptr || PyDict_Size(kwargs) == 0) && PyTuple_Size(args) == 1) {
+            //construct an alternative from a single argument.
+            //if it's a binary compatible subtype of the alternative we're constructing, then
+            //invoke the copy constructor.
+            PyObjectHolder arg(PyTuple_GetItem(args, 0));
+            Type* argType = extractTypeFrom(arg->ob_type);
+
+            if (argType && alt == argType) {
+                //it's already the right kind of instance, so we can copy-through the underlying element
+                alt->elementType()->copy_constructor(p, alt->eltPtr(((PyInstance*)(PyObject*)arg)->dataPtr()));
+                return;
+            }
+
+            //otherwise, if we have exactly one subelement, attempt to construct from that
+            if (alt->elementType()->getTypeCategory() != Type::TypeCategory::catNamedTuple) {
+                throw std::runtime_error("ConcreteAlternatives are supposed to only contain NamedTuples");
+            }
+
+            NamedTuple* alternativeEltType = (NamedTuple*)alt->elementType();
+
+            if (alternativeEltType->getTypes().size() != 1) {
+                throw std::logic_error("Can't initialize " + alt->name() + " with positional arguments because it doesn't have only one field.");
+            }
+
+            PyInstance::copyConstructFromPythonInstance(alternativeEltType->getTypes()[0], p, arg, true);
+        } else if (PyTuple_Size(args) == 0) {
+            //construct an alternative from Kwargs
+            constructFromPythonArguments(p, alt->elementType(), args, kwargs);
+        } else {
+            throw std::logic_error("Can only initialize " + alt->name() + " from python with kwargs or a single in-place argument");
+        }
+    });
+}
+
+void PyAlternativeInstance::copyConstructFromPythonInstanceConcrete(Alternative* altType, instance_ptr tgt, PyObject* pyRepresentation, bool isExplicit) {
+    Type* argType = extractTypeFrom(pyRepresentation->ob_type);
+
+    if (argType && argType->getTypeCategory() == Type::TypeCategory::catConcreteAlternative &&
+            ((ConcreteAlternative*)argType)->getAlternative() == altType) {
+        altType->copy_constructor(tgt, ((PyInstance*)pyRepresentation)->dataPtr());
+        return;
+    }
+
+    PyInstance::copyConstructFromPythonInstanceConcrete(altType, tgt, pyRepresentation, isExplicit);
+}
+
+
+PyObject* PyAlternativeInstance::tp_getattr_concrete(PyObject* pyAttrName, const char* attrName) {
+    if (mIsMatcher) {
+        if (type()->subtypes()[type()->which(dataPtr())].first == attrName) {
+            return incref(Py_True);
+        }
+        return incref(Py_False);
+    }
+
+    if (strcmp(attrName,"matches") == 0) {
+        PyInstance* self = duplicate();
+
+        self->mIteratorOffset = -1;
+        self->mIsMatcher = true;
+
+        return (PyObject*)self;
+    }
+
+    //see if its a method
+    Alternative* toCheck = type();
+
+    auto it = toCheck->getMethods().find(attrName);
+    if (it != toCheck->getMethods().end()) {
+        return PyMethod_New(
+            (PyObject*)it->second->getOverloads()[0].getFunctionObj(),
+            (PyObject*)this
+            );
+    }
+
+    //see if its a member of our held type
+    NamedTuple* heldT = (NamedTuple*)type()->subtypes()[type()->which(dataPtr())].second;
+    instance_ptr heldData = type()->eltPtr(dataPtr());
+
+    int ix = heldT->indexOfName(attrName);
+    if (ix >= 0) {
+        return extractPythonObject(
+            heldT->eltPtr(heldData, ix),
+            heldT->getTypes()[ix]
+            );
+    }
+
+    return PyInstance::tp_getattr_concrete(pyAttrName, attrName);
+}
+
+PyObject* PyConcreteAlternativeInstance::tp_getattr_concrete(PyObject* pyAttrName, const char* attrName) {
+    if (mIsMatcher) {
+        if (type()->getAlternative()->subtypes()[type()->getAlternative()->which(dataPtr())].first == attrName) {
+            return incref(Py_True);
+        }
+        return incref(Py_False);
+    }
+
+    if (strcmp(attrName,"matches") == 0) {
+        PyInstance* self = duplicate();
+
+        self->mIteratorOffset = -1;
+        self->mIsMatcher = true;
+
+        return (PyObject*)self;
+    }
+
+    //see if its a method
+    Alternative* toCheck = type()->getAlternative();
+
+    auto it = toCheck->getMethods().find(attrName);
+    if (it != toCheck->getMethods().end()) {
+        return PyMethod_New(
+            (PyObject*)it->second->getOverloads()[0].getFunctionObj(),
+            (PyObject*)this
+            );
+    }
+
+    //see if its a member of our held type
+    NamedTuple* heldT = (NamedTuple*)type()->getAlternative()->subtypes()[type()->which()].second;
+    instance_ptr heldData = type()->getAlternative()->eltPtr(dataPtr());
+
+    int ix = heldT->indexOfName(attrName);
+    if (ix >= 0) {
+        return extractPythonObject(
+            heldT->eltPtr(heldData, ix),
+            heldT->getTypes()[ix]
+            );
+    }
+
+    return PyInstance::tp_getattr_concrete(pyAttrName, attrName);
+}
+
+void PyAlternativeInstance::mirrorTypeInformationIntoPyTypeConcrete(Alternative* alt, PyTypeObject* pyType) {
+    PyObjectStealer alternatives(PyTuple_New(alt->subtypes().size()));
+
+    for (long k = 0; k < alt->subtypes().size(); k++) {
+        ConcreteAlternative* concrete = ConcreteAlternative::Make(alt, k);
+
+        PyDict_SetItemString(
+            pyType->tp_dict,
+            alt->subtypes()[k].first.c_str(),
+            (PyObject*)typeObjInternal(concrete)
+            );
+
+        PyTuple_SetItem(alternatives, k, incref((PyObject*)typeObjInternal(concrete)));
+    }
+
+    PyDict_SetItemString(
+        pyType->tp_dict,
+        "__typed_python_alternatives__",
+        alternatives
+        );
+}
+
+void PyConcreteAlternativeInstance::mirrorTypeInformationIntoPyTypeConcrete(ConcreteAlternative* alt, PyTypeObject* pyType) {
+    PyDict_SetItemString(
+        pyType->tp_dict,
+        "Alternative",
+        (PyObject*)typeObjInternal(alt->getAlternative())
+        );
+
+    PyDict_SetItemString(
+        pyType->tp_dict,
+        "Index",
+        PyLong_FromLong(alt->which())
+        );
+
+    PyDict_SetItemString(
+        pyType->tp_dict,
+        "Name",
+        PyUnicode_FromString(alt->getAlternative()->subtypes()[alt->which()].first.c_str())
+        );
+
+    PyDict_SetItemString(
+        pyType->tp_dict,
+        "ElementType",
+        (PyObject*)typeObjInternal(alt->elementType())
+        );
+
+}
+
+
+int PyAlternativeInstance::tp_setattr_concrete(PyObject* attrName, PyObject* attrVal) {
+    PyErr_Format(
+        PyExc_AttributeError,
+        "Cannot set attributes on instance of type '%s' because it is immutable",
+        type()->name().c_str()
+    );
+    return -1;
+}
+
+
+int PyConcreteAlternativeInstance::tp_setattr_concrete(PyObject* attrName, PyObject* attrVal) {
+    PyErr_Format(
+        PyExc_AttributeError,
+        "Cannot set attributes on instance of type '%s' because it is immutable",
+        type()->name().c_str()
+    );
+    return -1;
+}
+
+
+PyObject* PyConcreteAlternativeInstance::tp_call_concrete(PyObject* args, PyObject* kwargs) {
+    auto it = type()->getAlternative()->getMethods().find("__call__");
+
+    if (it == type()->getAlternative()->getMethods().end()) {
+        PyErr_Format(
+            PyExc_TypeError,
+            "'%s' object is not callable because '__call__' was not defined",
+            type()->name().c_str()
+            );
+        throw PythonExceptionSet();
+    }
+    // else
+    Function* f = it->second;
+    auto res = PyFunctionInstance::tryToCallAnyOverload(f, (PyObject*)this, args, kwargs);
+    if (res.first) {
+        return res.second;
+    }
+    // else
+    throw PythonExceptionSet();
+}
+
---
+++ typed_python/PyConstDictInstance.hpp
@@ -0,0 +1,59 @@
+/******************************************************************************
+   Copyright 2017-2019 Nativepython Authors
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+******************************************************************************/
+
+#pragma once
+
+#include "PyInstance.hpp"
+
+class PyConstDictInstance : public PyInstance {
+public:
+    typedef ConstDictType modeled_type;
+
+    ConstDictType* type();
+
+    PyObject* tp_iter_concrete();
+
+    PyObject* tp_iternext_concrete();
+
+    Py_ssize_t mp_and_sq_length_concrete();
+
+    int sq_contains_concrete(PyObject* item);
+
+    PyObject* pyOperatorConcrete(PyObject* rhs, const char* op, const char* opErr);
+
+    PyObject* mp_subscript_concrete(PyObject* item);
+
+    static PyObject* constDictItems(PyObject *o);
+
+    static PyObject* constDictKeys(PyObject *o);
+
+    static PyObject* constDictValues(PyObject *o);
+
+    static PyObject* constDictGet(PyObject* o, PyObject* args);
+
+    static PyMethodDef* typeMethodsConcrete(Type* t);
+
+    static void mirrorTypeInformationIntoPyTypeConcrete(ConstDictType* constDictT, PyTypeObject* pyType);
+
+    static bool pyValCouldBeOfTypeConcrete(modeled_type* type, PyObject* pyRepresentation, bool isExplicit);
+
+    static bool compare_to_python_concrete(ConstDictType* dictType, instance_ptr self, PyObject* other, bool exact, int pyComparisonOp);
+
+    static void copyConstructFromPythonInstanceConcrete(ConstDictType* dictType, instance_ptr tgt, PyObject* pyRepresentation, bool isExplicit);
+};
+
+
+

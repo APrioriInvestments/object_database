7e87bb26041a88acc072cb9c3e4edeea1fa6cacd
dkrasner <krasnerdaniel@gmail.com>
2019-10-12 15:30:22 -0400

    async message handler setup

--- object_database/web/content/AsyncMessageHandler.js
+++ object_database/web/content/AsyncMessageHandler.js
@@ -9,116 +9,52 @@
 
 class AsyncMessageHandler {
     constructor() {
-        this.queue = {};
+        this.queue = null;
         // this is where incoming messages that need to be cached for
         // later are stored
-        this.cache_queue = {};
+        this.cashe_queue = null;
 
         // Bind context to methods
         this.addToQueue = this.addToQueue.bind(this);
         this.removeFromQueue = this.removeFromQueue.bind(this);
-        this._addToCacheQueue = this._addToCacheQueue.bind(this);
-        this._removeFromCacheQueue = this._removeFromCacheQueue.bind(this);
+        this._addToCacheQueue = this._addToCacheQueue._bind(this);
+        this._removeFromCacheQueue = this._removeFromCacheQueue._bind(this);
     }
 
     /* I handle serve-side incoming messages and return a list
      * of messages (potenitally empty) to the CellHandler.
      */
     processMessage(message){
-        // messages to return
-        let messages = [];
-        // check if component/element id is in the queue
-        if (!this.queue[message.id]){
-            throw `Component id '${message.id}' has no messages in the queue.`
-        }
-        // check which order the current message_id takes in the queue for
-        // that component
-        let message_index = this.queue[message.id].indexOf(message.message_id);
-        if (message_index === -1){
-            throw `Message '${message.message_id}' not found in queue for component ${message.id}.`
-        } else if(message_index === 0){
-            // if the message_id is first in the queue return and all subsequent
-            // cached messages; remove the relevant message_ids from the queue and messages
-            // from the cache_queue
-            this.removeFromQueue(message);
-            messages.push(message);
-            for (let i = 0; i < this.queue[message.id].length; i++){
-                // if the next message_id in the queue corresponds to the next
-                // message in the cache queue then we add it to the list of messages
-                // to return and remove from the queues
-                let message_id = this.queue[message.id][i];
-                let cache_message_id = this.cache_queue[message.id][i].message_id;
-                if (message_id === cache_message_id){
-                    this.queue[message.id].splice(i, 1); //TODO standardize these removals;
-                    let next_message = this._removeFromCacheQueue(message.id, message_id);
-                    console.log(next_message);
-                    messages.push(next_message);
-                }
-            }
-        } else {
-            // if the message_id is not first in the queue cache the message for later
-            this._addToCacheQueue(message);
-        }
-        return messages;
     }
 
     addToQueue(message){
         // make sure the message has the component name
         // and a timestamp
-        if (!message.id) {
-            throw "Message missing component 'id'";
+        if (message.target_cell === null) {
+            throw "Message missing 'target_cell' id";
         }
-        if (!message.message_id) {
-            throw "Message missing 'message_id'";
-        }
-
-        if (!this.queue[message.id]){
-            this.queue[message.id] = [message.message_id]
-        } else {
-            this.queue[message.id].push(message.message_id)
+        if (message.message_id === null) {
+            throw "Message missing 'message_id' id";
         }
     }
 
     removeFromQueue(message){
         // make sure the message has the component name
         // and a timestamp
-        if (!message.id) {
-            throw "Message missing component 'id'";
+        if (message.target_cell === null) {
+            throw "Message missing 'target_cell' id";
         }
-        if (!message.message_id) {
-            throw "Message missing 'message_id'";
+        if (message.message_id === null) {
+            throw "Message missing 'message_id' id";
         }
-        if (!this.queue[message.id]){
-            throw `No messages in queue for component ${message.id}`
-        }
-        this.queue[message.id].map((item, index) => {
-            if (item === message.message_id){
-                this.queue[message.id].splice(index, 1);
-            }
-        })
     }
+
 
     _addToCacheQueue(message){
-        if (!this.cache_queue[message.id]){
-            this.cache_queue[message.id] = [message]
-        } else {
-            this.cache_queue[message.id].push(message)
-        }
     }
 
-    _removeFromCacheQueue(component_id, message_id){
-        let removed_message = null;
-        if (!this.cache_queue[component_id]){
-            throw `No messages in cache_queue for component ${component_id}`
-        }
-        this.cache_queue[component_id].map((item, index) => {
-            if (item.message_id === message_id){
-                removed_message = this.cache_queue[component_id][index];
-                this.cache_queue[component_id].splice(index, 1);
-            }
-        })
-        return removed_message;
+
+    _removeFromCacheQueue(message){
     }
+
 }
-
-export {AsyncMessageHandler, AsyncMessageHandler as default};

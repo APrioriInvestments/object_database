d9fecf497a675eca0ab4dd44824589df67acf947
dkrasner <dkrasner@users.noreply.github.com>
2019-10-18 16:40:04 +0200

    Revert "adding more tests for AsyncMessageHandler"
    This reverts commit 4267359ac318714ae289844ee71bb39ade98fbff.

--- object_database/web/content/tests/AsyncMessageHandlerTests.js
+++ object_database/web/content/tests/AsyncMessageHandlerTests.js
@@ -118,7 +118,7 @@ describe("Basic AsyncMessageHandler Tests", () => {
     it('Adding single message to cache queue', () => {
         let instance = new AsyncMessageHandler();
         assert.equal(instance.cache_queue.toString(), {}.toString());
-        instance._addToCacheQueue(basic_message1);
+        instance._addToCacheQueue(basic_message1.id, basic_message1.message_id);
         assert.equal(instance.cache_queue["component_1"].length, 1);
     });
     it('Remove single message from cache queue', () => {
@@ -126,66 +126,8 @@ describe("Basic AsyncMessageHandler Tests", () => {
         assert.equal(instance.cache_queue.toString(), {}.toString());
         instance._addToCacheQueue(basic_message1);
         assert.equal(instance.cache_queue["component_1"].length, 1);
-        let removed_message = instance._removeFromCacheQueue(basic_message1.id, basic_message1.message_id);
+        let removed_message = instance._removeFromCacheQueue(basic_message1);
         assert.equal(instance.cache_queue["component_1"].length, 0);
         assert.equal(removed_message.toString(), basic_message1.toString());
-    });
-    it('Process message (basic).', () => {
-        let instance = new AsyncMessageHandler();
-        assert.equal(instance.cache_queue.toString(), {}.toString());
-        instance.addToQueue(basic_message1);
-        assert.equal(instance.queue[basic_message1.id].length, 1);
-        let messages = instance.processMessage(basic_message1);
-        assert.equal(messages.length, 1);
-        assert.equal(instance.queue[basic_message1.id].length, 0);
-    });
-    it('Process message (add to cache queue).', () => {
-        let instance = new AsyncMessageHandler();
-        assert.equal(instance.cache_queue.toString(), {}.toString());
-        instance.addToQueue(basic_message1);
-        instance.addToQueue(basic_message11);
-        assert.equal(instance.queue[basic_message1.id].length, 2);
-        let messages = instance.processMessage(basic_message11);
-        assert.equal(messages.length, 0);
-        assert.equal(instance.cache_queue[basic_message11.id].length, 1);
-        assert.equal(instance.cache_queue[basic_message11.id].toString(), basic_message11.toString());
-    });
-    it('Process message (multiple messages).', () => {
-        let instance = new AsyncMessageHandler();
-        assert.equal(instance.cache_queue.toString(), {}.toString());
-        instance.addToQueue(basic_message1);
-        instance.addToQueue(basic_message11);
-        let messages = instance.processMessage(basic_message11);
-        assert.equal(messages.length, 0);
-        assert.equal(instance.queue[basic_message1.id].length, 2);
-        assert.equal(instance.cache_queue[basic_message1.id].length, 1);
-        messages = instance.processMessage(basic_message1);
-        // TODO: finish this test
-        // assert.equal(messages.length, 2);
-        // assert.equal(instance.queue[basic_message1.id].length, 0);
-        // assert.equal(instance.cache_queue[basic_message11.id].length, 0);
-        //assert.equal(instance.cache_queue[basic_message11.id].toString(), basic_message11.toString());
-    });
-    it('Process message error (component has no messages in queue).', () => {
-        let instance = new AsyncMessageHandler();
-        assert.equal(instance.cache_queue.toString(), {}.toString());
-        try {
-            instance.processMessage(basic_message1);
-        } catch(e) {
-            assert.equal(e, `Component id '${basic_message1.id}' has no messages in the queue.`);
-        }
-    });
-    it('Process message error (message id not foind in queue).', () => {
-        let instance = new AsyncMessageHandler();
-        assert.equal(instance.cache_queue.toString(), {}.toString());
-        instance.addToQueue(basic_message1);
-        assert.equal(instance.queue["component_1"].length, 1);
-        try {
-            instance.processMessage(basic_message11);
-        } catch(e) {
-            assert.equal(e,
-                `Message '${basic_message11.message_id}' not found in queue for component ${basic_message11.id}.`
-        );
-        }
     });
 });

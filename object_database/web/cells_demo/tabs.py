560080e704f18a523536a64fd33340f97ce550e2
Alexandros Tzannes <atzannes@gmail.com>
2019-12-03 17:22:46 -0500

    CellsTestPage tests for cells.Tabs

---
+++ object_database/web/cells_demo/tabs.py
@@ -0,0 +1,37 @@
+#   Copyright 2017-2019 object_database Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from object_database.web import cells as cells
+from object_database.web.CellsTestPage import CellsTestPage
+
+
+class TwoSimpleTabs(CellsTestPage):
+    def cell(self):
+        return cells.Tabs([("Tab1", "Tab1 Text"), ("Tab2", "Tab2 Text")])
+
+    def text(self):
+        return "Two tabs with text"
+
+
+class TwoSubscribedTabs(CellsTestPage):
+    def cell(self):
+        return cells.Tabs(
+            [
+                (cells.Subscribed(lambda: "Tab1"), "Tab1 Text"),
+                (cells.Subscribed(lambda: "Tab2"), "Tab2 Text"),
+            ]
+        )
+
+    def text(self):
+        return "Two tabs with text"

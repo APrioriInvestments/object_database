6a5ffeb04dd508b5d1b917ede6195896bcdbdcf6
Eric Gade <eric.gade@gmail.com>
2020-04-10 15:21:18 -0400

    Adding working implementation of intersection with tests
    Have also added a #union method to assist in the
    intersection calculations

--- object_database/web/content/webcomponents/sheet/src/Frame.js
+++ object_database/web/content/webcomponents/sheet/src/Frame.js
@@ -24,12 +24,14 @@ class Frame {
         validateGeometry(this.origin, this.corner);
 
         this.isFrame = true;
+        this.isEmpty = false;
 
         // Bind instance methods
         this.contains = this.contains.bind(this);
         this.equals = this.equals.bind(this);
         this.translate = this.translate.bind(this);
         this.intersection = this.intersection.bind(this);
+        this.union = this.union.bind(this);
         this.forEachPoint = this.forEachPoint.bind(this);
         this.forEachCoordinate = this.forEachCoordinate.bind(this);
         this.mapEachPoint = this.mapEachPoint.bind(this);
@@ -48,6 +50,9 @@ class Frame {
      * @returns {boolean}
      */
     contains(aPointOrFrame){
+        if(this.isEmpty){
+            return false;
+        }
         if(aPointOrFrame.isPoint){
             let validX = (aPointOrFrame.x >= this.origin.x && aPointOrFrame.x <= this.corner.x);
             let validY = (aPointOrFrame.y >= this.origin.y && aPointOrFrame.y <= this.corner.y);
@@ -67,6 +72,11 @@ class Frame {
      * @returns {boolean}
      */
     equals(otherFrame){
+        if(this.isEmpty && otherFrame.isEmpty){
+            return true;
+        } else if(this.isEmpty || otherFrame.isEmpty){
+            return false;
+        }
         return (this.origin.equals(otherFrame.origin)
                 && this.corner.equals(otherFrame.corner));
     }
@@ -87,6 +97,12 @@ class Frame {
      * @returns {Frame}
      */
     translate(translationPoint, inPlace=true){
+        if(this.isEmpty){
+            if(inPlace){
+                return this;
+            }
+            return this.constructor.newEmpty();
+        }
         if(inPlace){
             let newFrame = new Frame(this.origin, this.corner);
             newFrame.translate(translationPoint, false);
@@ -112,22 +128,122 @@ class Frame {
         }
 
         if(this.contains(otherFrame)){
+            /*
+             * TTTTTTTT
+             * TTOOOTTT
+             * TTOOOTTT
+             * TTOOOTTT
+             * TTTTTTTT
+             */
             return new Frame(otherFrame.origin, otherFrame.corner);
         }
 
         if(otherFrame.contains(this)){
+            /*
+             * OOOOOOOO
+             * OOTTTOOO
+             * OOTTTOOO
+             * OOTTTOOO
+             * OOOOOOOO
+             */
             return new Frame(this.origin, this.corner);
         }
 
-        if(this.contains(otherFrame.origin)){
+        if(this.contains(otherFrame.origin) && otherFrame.contains(this.corner)){
+            /*
+              * TTTTTT
+              * TTTOOOOOO
+              * TTTOOOOOO
+              * TTTOOOOOO
+              *    OOOOOO
+              */
             return new Frame(otherFrame.origin, this.corner);
         }
 
-        if(otherFrame.contains(this.origin)){
+        if(otherFrame.contains(this.origin) && this.contains(otherFrame.corner)){
+            /*
+              * OOOOOO
+              * OOOOOO
+              * OOOTTTTTT
+              * OOOTTTTTT
+              *    TTTTTT
+              *    TTTTTT
+              */
             return new Frame(this.origin, otherFrame.corner);
         }
 
+        // If we get here, we possibly have a more complex
+        // intersection (at top right, bottom left, etc).
+        // We create a union frame and then get the min and
+        // max for all intersection points
+        let unionFrame = this.union(otherFrame);
+        let intersectPoints = unionFrame.points.filter(aPoint => {
+            return (this.contains(aPoint) && otherFrame.contains(aPoint));
+        });
+        if(intersectPoints.length > 0){
+            let xVals = intersectPoints.map(aPoint => {
+                return aPoint.x;
+            });
+            let yVals = intersectPoints.map(aPoint => {
+                return aPoint.y;
+            });
+            let newOrigin = new Point([
+                Math.min(...xVals),
+                Math.min(...yVals)
+            ]);
+            let newCorner = new Point([
+                Math.max(...xVals),
+                Math.max(...yVals)
+            ]);
+            return new Frame(newOrigin, newCorner);
+        }
+
         return this.constructor.newEmpty();
+    }
+
+    /**
+     * Returns a new Frame instance that
+     * encloses the total areas of both
+     * this frame and the incoming frame
+     * TODO: See about refactoring
+     * using min()/max()
+     */
+    union(otherFrame){
+        let cornerX;
+        let cornerY;
+        let originX;
+        let originY;
+
+        // Set the new origin to be the min
+        // of the possible x and y values for
+        // both
+        if(this.origin.x <= otherFrame.origin.x){
+            originX = this.origin.x;
+        } else {
+            originX = otherFrame.origin.x;
+        }
+        if(this.origin.y <= otherFrame.origin.y){
+            originY = this.origin.y;
+        } else {
+            originY = otherFrame.origin.y;
+        }
+
+        // Set the new corner to be the max of
+        // the possible x, y values for both
+        if(this.corner.x >= otherFrame.corner.x){
+            cornerX = this.corner.x;
+        } else {
+            cornerX = otherFrame.corner.x;
+        }
+        if(this.corner.y >= otherFrame.corner.y){
+            cornerY = this.corner.y;
+        } else {
+            cornerY = otherFrame.corner.y;
+        }
+        return new Frame(
+            [originX, originY],
+            [cornerX, cornerY]
+        );
     }
 
     /**
@@ -137,6 +253,9 @@ class Frame {
      * to call over each point.
      */
     forEachPoint(callback){
+        if(this.isEmpty){
+            return;
+        }
         for(let x = this.origin.x; x <= this.corner.x; x++){
             for(let y = this.origin.y; y <= this.corner.y; y++){
                 callback(new Point([x,y]));
@@ -170,6 +289,9 @@ class Frame {
      * to call on each coordinate array
      */
     forEachCoordinate(callback){
+        if(this.isEmpty){
+            return;
+        }
         for(let x = this.origin.x; x <= this.corner.x; x++){
             for(let y = this.origin.y; y <= this.corner.y; y++){
                 callback([x, y]);
@@ -196,6 +318,9 @@ class Frame {
     }
 
     forEachCoordinateRow(callback){
+        if(this.isEmpty){
+            return;
+        }
         for(let y = this.origin.y; y <= this.corner.y; y++){
             let row = [];
             for(let x = this.origin.x; x <= this.corner.x; x++){
@@ -222,9 +347,11 @@ class Frame {
      */
     get points(){
         let points = [];
-        for(let x = this.origin.x; x <= this.corner.x; x++){
-            for(let y = this.origin.y; y <= this.corner.y; y++){
-                points.push(new Point([x, y]));
+        if(!this.isEmpty){
+            for(let x = this.origin.x; x <= this.corner.x; x++){
+                for(let y = this.origin.y; y <= this.corner.y; y++){
+                    points.push(new Point([x, y]));
+                }
             }
         }
         return points;
@@ -238,11 +365,14 @@ class Frame {
      */
     get coordinates(){
         let coordinates = [];
-        for(let x = this.origin.x; x <= this.corner.x; x++){
-            for(let y = this.origin.y; y <= this.corner.y; y++){
-                coordinates.push([x, y]);
+        if(!this.isEmpty){
+            for(let x = this.origin.x; x <= this.corner.x; x++){
+                for(let y = this.origin.y; y <= this.corner.y; y++){
+                    coordinates.push([x, y]);
+                }
             }
         }
+        return coordinates;
     }
 
     /**
@@ -262,11 +392,33 @@ class Frame {
     }
 
     /**
+     * Return the number of dimensions for
+     * the current Frame.
+     * Note that a Frame with identical
+     * origin and corner is not necessarily
+     * 'empty' (0-dimensional), as it can
+     * contain a single point (cell) which
+     * is actually 1 dimensional.
+     */
+    get dimensions(){
+        if(this.isEmpty){
+            return 0;
+        }
+        if(this.origin.equals(this.corner)){
+            return 1;
+        }
+        return 2;
+    }
+
+    /**
      * Returns the total number of contained
      * Points/coordinates given the current
      * origin and corner values.
      */
     get area(){
+        if(this.isEmpty){
+            return 0;
+        }
         let maxX = (this.corner.x - this.origin.x) - 1;
         let maxY = (this.corner.y - this.origin.y) - 1;
         return maxX * maxY;
@@ -311,26 +463,69 @@ class Frame {
     }
 
     /**
-     * Returns true if the Frame is "empty",
-     * meaning that is has no dimensions and
-     * the origin and corner are identical.
-     */
-    get isEmpty(){
-        if(this.origin.x == this.corner.x && this.origin.y == this.corner.y){
-            return true;
-        }
-        return false;
-    }
-
-    /**
      * Class method for constructing a new
      * empty Frame.
      */
     static newEmpty(){
-        return new this(
-            [0,0],
-            [0,0]
-        );
+        let newFrame = new this([0,0], [0,0]);
+        newFrame.isEmpty = true;
+        return newFrame;
+    }
+
+    /**
+     * Creates a new Frame instance based on two
+     * Points. Unlike the regular constructor, these
+     * points to not have to be the origin and corner,
+     * but could be any of the four corners.
+     */
+    static fromPointToPoint(firstPoint, secondPoint){
+        if(firstPoint.equals(secondPoint)){
+            return new Frame(firstPoint, secondPoint);
+        }
+        let newOrigin;
+        let newCorner;
+        if(firstPoint.x < secondPoint.x){
+            // In this case, the firstPoint
+            // is the bottomLeft and the
+            // secondPoint is the topRight
+            if(firstPoint.y > secondPoint.y){
+                newOrigin = new Point([
+                    firstPoint.x,
+                    secondPoint.y
+                ]);
+                newCorner = new Point([
+                    secondPoint.x,
+                    firstPoint.y
+                ]);
+                return new Frame(newOrigin, newCorner);
+            }
+        }
+        if(firstPoint.x > secondPoint.x){
+            // In this case, the firstPoint
+            // is the topRight and the
+            // secondPoint is bottomLeft
+            if(firstPoint.y < secondPoint.y){
+                newOrigin = new Point([
+                    secondPoint.x,
+                    firstPoint.y
+                ]);
+                newCorner = new Point([
+                    firstPoint.x,
+                    secondPoint.y
+                ]);
+                return new Frame(newOrigin, newCorner);
+                // Otherwise the secondPoint is the same as
+                // the origin and the first is the same
+                // as the corner
+            } else {
+                return new Frame(secondPoint, firstPoint);
+            }
+        }
+
+        // If we get here, then the firstPoint
+        // is the origin and the second is the
+        // corner
+        return new Frame(firstPoint, secondPoint);
     }
 }
 

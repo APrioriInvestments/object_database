3529a1067efe6ab3088a635176be7ef583691bff
dkrasner <krasnerdaniel@gmail.com>
2020-04-16 08:07:43 -0400

    adding a keyregistry WS request demo and refactoring keyaction cell and component

---
+++ object_database/web/cells_demo/keyregistry.py
@@ -0,0 +1,36 @@
+#   Copyright 2017-2019 object_database Authors
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+from object_database.web import cells as cells
+from object_database.web.CellsTestPage import CellsTestPage
+
+
+class KeyRegistryInfoRequest(CellsTestPage):
+    """An example of sending a WS message to get the KeyRegistry info."""
+
+    def cell(self):
+        message = {"event": "WSTest", "type": "WS message test"}
+
+        messageSlot = cells.Slot("Waiting for a message")
+
+        def callBack(info):
+            messageSlot.set(info)
+
+        return cells.ResizablePanel(
+            cells.WSMessageTester(WSMessageToSend=message, onCallbackFunc=callBack),
+            cells.Subscribed(lambda: cells.Text(messageSlot.get())),
+        )
+
+    def text(self):
+        return ""

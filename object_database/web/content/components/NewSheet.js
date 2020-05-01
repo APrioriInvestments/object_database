bce3f01e130b871534f9f85064edc1ffdd1e7d0d
Eric Gade <eric.gade@gmail.com>
2020-05-01 12:46:21 -0400

    Complete Selector key events implemented in NewSheet Component

--- object_database/web/content/components/NewSheet.js
+++ object_database/web/content/components/NewSheet.js
@@ -43,9 +43,21 @@ class NewSheet extends Component {
         // Bind component event handlers
         this.onPageUp = this.onPageUp.bind(this);
         this.onPageDown = this.onPageDown.bind(this);
+        this.onPageRight = this.onPageRight.bind(this);
+        this.onPageLeft = this.onPageLeft.bind(this);
+        this.onSelectPageUp = this.onSelectPageUp.bind(this);
+        this.onSelectPageDown = this.onSelectPageDown.bind(this);
+        this.onSelectPageRight = this.onSelectPageRight.bind(this);
+        this.onSelectPageLeft = this.onSelectPageLeft.bind(this);
         this.onArrowUp = this.onArrowUp.bind(this);
+        this.onSelectArrowUp = this.onSelectArrowUp.bind(this);
+        this.onUpToTop = this.onUpToTop.bind(this);
         this.onArrowDown = this.onArrowDown.bind(this);
+        this.onSelectArrowDown = this.onSelectArrowDown.bind(this);
+        this.onDownToBottom = this.onDownToBottom.bind(this);
         this.onArrowRight = this.onArrowRight.bind(this);
+        this.onSelectArrowRight = this.onSelectArrowRight.bind(this);
+        this.onOverToRight = this.onOverToRight.bind(this);
         this.onArrowLeft = this.onArrowLeft.bind(this);
     }
 
@@ -60,12 +72,174 @@ class NewSheet extends Component {
     setupEvents(){
         let myElement = this.getDOMElement();
         this.keyBindings = [
-            new KeyBinding('PageUp', this.onPageUp),
-            new KeyBinding('PageDown', this.onPageDown),
-            new KeyBinding('ArrowUp', this.onArrowUp),
-            new KeyBinding('ArrowDown', this.onArrowDown),
-            new KeyBinding('ArrowRight', this.onArrowRight),
-            new KeyBinding('ArrowLeft', this.onArrowLeft)
+            new KeyBinding(
+                'PageUp',
+                this.onPageUp,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'PageDown',
+                this.onPageDown,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'altKey+PageUp',
+                this.onPageLeft,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'altKey+PageDown',
+                this.onPageRight,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+PageUp',
+                this.onSelectPageUp,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+PageDown',
+                this.onSelectPageDown,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+altKey+PageUp',
+                this.onSelectPageLeft,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+altKey+PageDown',
+                this.onSelectPageRight,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ArrowUp',
+                this.onArrowUp,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+ArrowUp',
+                this.onSelectArrowUp,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+ArrowUp',
+                this.onUpToTop,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+shiftKey+ArrowUp',
+                this.onSelectUpToTop,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ArrowDown',
+                this.onArrowDown,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+ArrowDown',
+                this.onSelectArrowDown,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+ArrowDown',
+                this.onDownToBottom,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+shiftKey+ArrowDown',
+                this.selectDownToBottom,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ArrowRight',
+                this.onArrowRight,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+ArrowRight',
+                this.onSelectArrowRight,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+ArrowRight',
+                this.onOverToRight,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+shiftKey+ArrowRight',
+                this.onSelectOverToRight,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ArrowLeft',
+                this.onArrowLeft,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'shiftKey+ArrowLeft',
+                this.onSelectArrowLeft,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+ArrowLeft',
+                this.onOverToLeft,
+                true,
+                true,
+                true
+            ),
+            new KeyBinding(
+                'ctrlKey+shiftKey+ArrowLeft',
+                this.onSelectOverToLeft,
+                true,
+                true,
+                true
+            )
         ];
         this.keyListener = new KeyListener(myElement, this.keyBindings);
         this.keyListener.start();
@@ -102,31 +276,119 @@ class NewSheet extends Component {
     /* Event Handlers */
 
     onPageUp(event){
-        console.log(event);
+        console.log('PageUp');
+        event.target.selector.pageUp();
+    }
+
+    onSelectPageUp(event){
+        console.log('PageUp selecting');
+        event.target.selector.pageUp(true);
+    }
+
+    onPageRight(event){
+        console.log('PageRight');
+        event.target.selector.pageRight();
+    }
+
+    onSelectPageRight(event){
+        console.log('PageRight selecting');
+        event.target.selector.pageRight(true);
+    }
+
+    onPageLeft(event){
+        console.log('PageLeft');
+        event.target.selector.pageLeft();
+    }
+
+    onSelectPageLeft(event){
+        console.log('PageLeft selecting');
+        event.target.selector.pageLeft(true);
     }
 
     onPageDown(event){
-        console.log(event);
+        console.log('PageDown');
+        event.target.selector.pageDown();
+    }
+
+    onSelectPageDown(event){
+        console.log('PageDown selecting');
+        event.target.selector.pageDown(true);
     }
 
     onArrowUp(event){
-        let selecting = event.shiftKey;
-        event.target.selector.moveUpBy(1, selecting);
+        console.log('ArrowUp');
+        event.target.selector.moveUpBy(1);
+    }
+
+    onSelectArrowUp(event){
+        console.log('ArrowUp selecting');
+        event.target.selector.moveUpBy(1, true);
     }
 
     onArrowDown(event){
-        let selecting = event.shiftKey;
-        event.target.selector.moveDownBy(1, selecting);
+        console.log('ArrowDown');
+        event.target.selector.moveDownBy(1);
+    }
+
+    onSelectArrowDown(event){
+        console.log('ArrowDown selecting');
+        event.target.selector.moveDownBy(1, true);
     }
 
     onArrowLeft(event){
-        let selecting = event.shiftKey;
-        event.target.selector.moveLeftBy(1, selecting);
+        console.log('ArrowLeft');
+        event.target.selector.moveLeftBy(1);
+    }
+
+    onSelectArrowLeft(event){
+        console.log('ArrowLeft selecting');
+        event.target.selector.moveLeftBy(1, true);
     }
 
     onArrowRight(event){
-        let selecting = event.shiftKey;
-        event.target.selector.moveRightBy(1, selecting);
+        console.log('ArrowRight');
+        event.target.selector.moveRightBy(1);
+    }
+
+    onSelectArrowRight(event){
+        console.log('ArrowRight selecting');
+        event.target.selector.moveRightBy(1, true);
+    }
+
+    onUpToTop(event){
+        console.log('moveToTopEnd');
+        event.target.selector.moveToTopEnd();
+    }
+
+    onSelectUpToTop(event){
+        event.target.selector.moveToTopEnd(true);
+    }
+
+    onDownToBottom(event){
+        console.log('moveToBottomEnd');
+        event.target.selector.moveToBottomEnd();
+    }
+
+    onSelecteDownToBottom(event){
+        event.target.selector.moveToBottomEnd(true);
+    }
+
+    onOverToRight(event){
+        console.log('moveToRightEnd');
+        event.target.selector.moveToRightEnd();
+    }
+
+    onSelectOverToRight(event){
+        event.target.selector.moveToRightEnd(true);
+    }
+
+    onOverToLeft(event){
+        console.log('moveToLeftEnd');
+        event.target.selector.moveToLeftEnd();
+    }
+
+    onSelectOverToLeft(event){
+        event.target.selector.moveToLeftEnd(true);
     }
 };
 

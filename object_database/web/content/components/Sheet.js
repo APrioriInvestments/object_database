d291a6dcfa12729e4b83731c80eff04e1d285dc3
Eric Gade <eric.gade@gmail.com>
2020-05-12 15:58:57 -0400

    [BUGFIX] Fixing mouse events to work with new inner div elements
    Previously our td elements were capturing all mousemove events.
    Now that we've added inner div elements for styling reasons,
    we needed to adjust the mouse event handlers to deal with these
    elements rather than the td elements directly.

--- object_database/web/content/components/Sheet.js
+++ object_database/web/content/components/Sheet.js
@@ -636,11 +636,11 @@ class Sheet extends Component {
     /* Mouse Event Handlers */
     onMouseDown(event){
         let primaryButtonPushed = event.button == 0;
-        if(event.target.tagName == "TD" && primaryButtonPushed){
+        if(event.target.matches('.sheet-cell-inner') && primaryButtonPushed){
             this._mouseIsDown = true;
             let sheet = this.getDOMElement();
-            sheet.selector.setAnchorToElement(event.target);
-            sheet.selector.setCursorToElement(event.target);
+            sheet.selector.setAnchorToElement(event.target.parentElement);
+            sheet.selector.setCursorToElement(event.target.parentElement);
             sheet.selector.updateElements();
 
             // Update the header
@@ -664,9 +664,9 @@ class Sheet extends Component {
     }
 
     onMouseMove(event, foo){
-        if(this._mouseIsDown && event.target.tagName =="TD"){
+        if(this._mouseIsDown && event.target.matches('.sheet-cell-inner')){
             let sheet = this.getDOMElement();
-            sheet.selector.setCursorToElement(event.target);
+            sheet.selector.setCursorToElement(event.target.parentElement);
             sheet.selector.selectFromAnchorTo(
                 sheet.selector.relativeCursor
             );

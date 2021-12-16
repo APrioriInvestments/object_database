2b1a50a69adf069b5f2d31593af161dd0fbe4c29
Alexandros Tzannes <alexandros.tzannes@aprioriinvestments.com>
2021-12-16 18:09:00 -0500

    ServiceBase provides an overridable configureLogging method

--- object_database/service_manager/ServiceBase.py
+++ object_database/service_manager/ServiceBase.py
@@ -43,6 +43,11 @@ class ServiceBase:
         self.runtimeConfig = runtimeConfig
         self.registeredReactors = []
         self._reactorsRunning = False
+        self.configureLogging()
+
+    def configureLogging(self):
+        """ Subclasses may override this method to configure logging. """
+        pass
 
     @staticmethod
     def configureFromCommandline(db, serviceObject, args):

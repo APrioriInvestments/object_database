b0cf197a5500e3e85df83eeee5d1cd28bfbfcb0c
Chang She <cshe@tubi.tv>
2022-05-10 13:32:46 -0700

    lint fix

--- docs/examples/producer.py
+++ docs/examples/producer.py
@@ -3,10 +3,12 @@ import time
 
 schema = Schema("hello_world")
 
+
 @schema.define
 class Message:
     timestamp = float
     message = str
+
 
 db = connect('localhost', 8000, 'TOKEN')
 db.subscribeToSchema(schema)

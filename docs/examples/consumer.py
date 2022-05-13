2fbafa76044ecbe27ca8b963cceb91c2e1f64f9e
dkrasner <krasnerdaniel@gmail.com>
2022-05-13 19:40:48 +0200

    fixing typo in docs/examples/consumer.py

--- docs/examples/consumer.py
+++ docs/examples/consumer.py
@@ -3,19 +3,20 @@ import time
 
 schema = Schema("hello_world")
 
+
 @schema.define
 class Message:
     timestamp = float
     message = str
 
-db = connect('localhost', 8000, 'TOKEN')
+
+db = connect("localhost", 8000, "TOKEN")
 db.subscribeToSchema(schema)
 
 ts = 0
 while True:
-    with db.view():
-        Message(timestamp=time.time(),
-                message='message_5')
+    with db.transaction():
+        Message(timestamp=time.time(), message="message_5")
         messages = Message.lookupAll()
         for m in messages:
             if m.timestamp > ts:

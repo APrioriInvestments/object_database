d63e4c09cd6fc9ea182000b1a493e0305dc5b41a
dkrasner <krasnerdaniel@gmail.com>
2022-06-02 15:26:58 +0200

    wrapping up examples/cells.py and adding examples/cells_odb.py

--- docs/examples/cells.py
+++ docs/examples/cells.py
@@ -9,8 +9,12 @@ class SomethingMoreInteresting(ServiceBase):
 
     @staticmethod
     def serviceDisplay(serviceObject, instance=None, objType=None, queryArgs=None):
-        return cells.Card(cells.button("Click me", onClick))
-
-
-def onClick():
-    return "thanks"
+        return cells.Card(
+            cells.Panel(
+                cells.Button("Reload", lambda: "") +
+                cells.Button("Service Base", lambda: "ServiceBase") +
+                cells.Button("Active Web Service", lambda: "ActiveWebService")
+            ),
+            header="This is a 'card' cell with some buttons",
+            padding="10px"
+        )

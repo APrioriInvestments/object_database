947d46fb1008a631e1f3ab28cec549b0b5288423
dkrasner <krasnerdaniel@gmail.com>
2022-11-07 21:12:42 +0100

    [WIP] rewriting Tree as regular object to dealing with leaderlines

--- object_database/web/devtools/tree/Tree.js
+++ object_database/web/devtools/tree/Tree.js
@@ -16,6 +16,13 @@ const templateString = `
 .depth {
     display: flex;
     justify-content: space-around;
+    border: 2px black solid;
+}
+
+.child-wrapper {
+    display: flex;
+    justify-content: space-around;
+    border: 1px blue solid;
 }
 
 </style>
@@ -33,42 +40,68 @@ class Tree extends HTMLElement {
         // bind methods
         this.setup = this.setup.bind(this);
         this.setupNode = this.setupNode.bind(this);
+        this.setupLeaderLines = this.setupLeaderLines.bind(this);
     }
 
     connectedCallback(){
         if(this.isConnected){
-            this.setup();
+            // this.setup();
         }
     }
 
     setup(data){
-        const wrapper = this.shadowRoot.querySelector("#wrapper");
+        // const wrapper = this.shadowRoot.querySelector("#wrapper");
+        const wrapper = document.body;
         const nodeDepth = document.createElement("div");
         nodeDepth.classList.add("depth");
+        nodeDepth.setAttribute("id", "depth-0");
         wrapper.append(nodeDepth);
-        this.setupNode(data, nodeDepth);
+        const nodeWrapper = document.createElement("div");
+        nodeWrapper.classList.add("child-wrapper");
+        nodeDepth.append(nodeWrapper);
+        this.setupNode(data, nodeWrapper, 1);
     }
 
-    setupNode(nodeData, nodeDepth){
+    setupNode(nodeData, wrapperDiv, depth){
         if(nodeData){
+            console.log(depth);
             const node = document.createElement("tree-node");
             node.name = nodeData.name;
             node.setAttribute("id", nodeData.id);
-            nodeDepth.append(node);
+            wrapperDiv.append(node);
             // setup the children in a new node depth
-            const wrapper = this.shadowRoot.querySelector("#wrapper");
-            const childrenDepth = document.createElement("div");
-            childrenDepth.classList.add("depth");
-            wrapper.append(childrenDepth);
-            nodeData.children.forEach((childData) => {
-                this.setupNode(childData, childrenDepth);
+            if(nodeData.children.length){
+                // if the corresponding depth has not been added, do so now
+                let depthDiv = document.body.querySelector(`#depth-${depth}`);
+                if (!depthDiv) {
+                    depthDiv = document.createElement("div");
+                    depthDiv.setAttribute("id", `depth-${depth}`);
+                    depthDiv.classList.add("depth");
+                    // const wrapper = this.shadowRoot.querySelector("#wrapper");
+                    const wrapper = document.body;
+                    wrapper.append(depthDiv);
+                }
+                const childWrapper = document.createElement("div");
+                childWrapper.classList.add("child-wrapper");
+                depthDiv.append(childWrapper);
+                nodeData.children.forEach((childData) => {
+                    const child = this.setupNode(childData, childWrapper, depth + 1);
+                    // this.updateLeaderLines(node, child);
 
-            })
+                })
+            }
+            return node;
         }
     }
 
-    updateLeaderLines(depth){
-        //TODO:
+    setupLeaderLines(data){
+        if (data && data.children) {
+            const parent = document.body.querySelector(`tree-node#${data.id}`);
+            data.children.forEach((cdata) => {
+                const child = document.body.querySelector(`tree-node#${cdata.id}`);
+                new LeaderLine(parent, child);
+            })
+        }
     }
 
     get ok(){

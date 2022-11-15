a94d0cff19367f81a280b0d354582e8f7c3916a4
dkrasner <krasnerdaniel@gmail.com>
2022-11-15 20:23:33 +0100

    leadrlines use proper js document.createElementNS API
    adding tests for the tree component

--- object_database/web/devtools/tree/Tree.js
+++ object_database/web/devtools/tree/Tree.js
@@ -108,6 +108,8 @@ class Tree extends HTMLElement {
     /**
       * I add an SVG bezier curve which starts at the bottom middle
       * of the startNode and ends at the top middle of the endNode
+      * NODE: svg-type elemnents need to be created using the
+      * SVG name space, ie document.createElementNS...
       * @param {element} startNode
       * @param {element} endNode
       */
@@ -121,31 +123,33 @@ class Tree extends HTMLElement {
         const endY = endRect.top;
         const endX = endRect.left + (endRect.width / 2);
 
+        const svg = document.createElementNS('http://www.w3.org/2000/svg', "svg");
+        svg.setAttribute("width", "100%");
+        svg.setAttribute("height", "100%");
+        svg.setAttribute("data-start-node-id", startNode.id);
+        svg.setAttribute("data-end-node-id", endNode.id);
+        svg.style.left =  0;
+        svg.style.top = 0;
+
+        const line = document.createElementNS('http://www.w3.org/2000/svg', "line");
+        line.setAttribute("x1", `${startX}`);
+        line.setAttribute("y1", `${startY}`);
+        line.setAttribute("x2", `${endX}`);
+        line.setAttribute("y2", `${endY}`);
+        line.setAttribute("stroke", "black");
+        svg.append(line);
+        wrapper.append(svg);
+        /*
+        const path = document.createElementNS('http://www.w3.org/2000/svg', "path");
+        path.setAttribute("d", `M ${startX} ${startY}  Q 95 10, ${endX} ${endY}`);
+        path.setAttribute("stroke", "black");
+        path.setAttribute("fill", "transparent");
+        svg.append(path);
+
+        wrapper.append(svg);
         const temp = document.createElement("div");
         temp.innerHTML = `<svg width="100%" height="100%" style="position: absolute; top: 0; left: 0"><line x1="${startX}" y1="${startY}" x2="${endX}" y2="${endY}" stroke="black"/></svg>`;
         wrapper.append(temp.childNodes[0])
-        /*
-        const svg = document.createElement("svg");
-        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-        svg.setAttribute("width", "100");
-        svg.setAttribute("height", "100");
-        svg.style.left =  `${startX}px`;
-        svg.style.top = `${startY}px`;
-        svg.style.position = "absolute";
-
-        const path = document.createElement("path");
-        path.setAttribute("d", `M ${startX} ${startY}  C 20 20, 40 20, ${endX} ${endY}`);
-        path.setAttribute("stroke", "black");
-        path.setAttribute("fill", "transparent");
-        //svg.append(path);
-
-        const line = document.createElement("line");
-        line.setAttribute("x1", "100");
-        line.setAttribute("y1", "0");
-        line.setAttribute("x2", "0");
-        line.setAttribute("y2", "100");
-        svg.append(line);
-        wrapper.append(svg);
         */
     }
 
--- object_database/web/devtools/tree/tests/component.js
+++ object_database/web/devtools/tree/tests/component.js
@@ -3,16 +3,80 @@
   **/
 
 import chai from "chai";
+import crypto from 'crypto';
+import { Tree } from '../tree.js';
 const assert = chai.assert;
 
-import { Tree } from '../tree.js';
+const treeMaker = function(depth, maxChildNum){
+    let rootNode = {
+        name: "root",
+        id: "roodID",
+        order: -1,
+        children: []
+    };
+    rootNode = addChildren(rootNode, 0, depth, maxChildNum);
+    return rootNode;
+}
+
+const addChildren = (node, depth, totalDepth, maxChildNum) => {
+    const numChildren = Math.ceil(Math.random()* maxChildNum);
+    if (depth <= totalDepth) {
+        for (let i = 1; i <= numChildren; i++){
+            const child = {
+                name: `node_${depth}_${i}`,
+                id: "id-" + crypto.randomUUID(),
+                order: i,
+                children: []
+            };
+            node.children.push(
+                addChildren(child, depth + 1, totalDepth, maxChildNum)
+            );
+
+        }
+    }
+    return node;
+}
+
 
 describe("Tree Component Tests", () => {
-    const tree = document.createElement("my-tree");
-    beforeEach(() => {
+    let tree;
+    let data
+    before(() => {
+        tree = document.createElement("tree-graph");
+        data = treeMaker(3, 2);
+        tree.setup(data);
+        document.body.append(tree);
     });
-    it("Ok", () => {
-        assert.equal(tree.ok, "ok");
+    it("Assert the tree exists", () => {
+        assert.exists(tree);
+    });
+    it("Tree has a node for each element in the data", () => {
+        const nodeCheck = (nodeData) => {
+            if (nodeData) {
+                const node = tree.shadowRoot.querySelector(`#${nodeData.id}`);
+                assert.exists(node);
+                nodeData.children.forEach((childData) => {
+                    const childNode = tree.shadowRoot.querySelector(`#${childData.id}`);
+                    assert.exists(childNode);
+                    nodeCheck(childData);
+                })
+            }
+        }
+        nodeCheck(data);
+    });
+    it("Tree has leaderlines for every parent-child node", () => {
+        const leaderlineCheck = (nodeData) => {
+            if (nodeData) {
+                nodeData.children.forEach((childData) => {
+                    const svg = tree.shadowRoot.querySelector(
+                        `svg[data-start-node-id="${nodeData.id}"][data-end-node-id="${childData.id}"]`
+                    );
+                    assert.exists(svg);
+                    leaderlineCheck(childData);
+                })
+            }
+        }
+        leaderlineCheck(data);
     });
 })
 

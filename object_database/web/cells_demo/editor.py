68c96214786653c28dc7db0da6ce7b7bf71848cf
Bok Young Kim <bokyoung.kim@aprioriinvestments.com>
2023-04-24 14:35:33 +0000

    Add demo and testing for triple clicking in Editor

--- object_database/web/cells_demo/editor.py
+++ object_database/web/cells_demo/editor.py
@@ -12,6 +12,8 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
+import re
+import time
 from object_database.web import cells as cells
 from object_database.web.CellsTestPage import CellsTestPage
 
@@ -150,7 +152,7 @@ class EditorWithAutocomplete(CellsTestPage):
         )
 
     def text(self):
-        return "You should an editor with autocompletion available."
+        return "You should see an editor with autocompletion available."
 
 
 class EditorWithNeverResolvedAutocomplete(CellsTestPage):
@@ -158,7 +160,7 @@ class EditorWithNeverResolvedAutocomplete(CellsTestPage):
         return cells.Editor(autocompleteFunction=lambda request: None)
 
     def text(self):
-        return "You should an editor with autocompletion available."
+        return "You should see an editor with autocompletion available."
 
 
 class EditorWithDoubleClick(CellsTestPage):
@@ -173,6 +175,115 @@ class EditorWithDoubleClick(CellsTestPage):
 
     def text(self):
         return (
-            "You should an editor which appends some line and column info if you double-click"
-            " any of its text"
+            "You should see an editor which appends some line and column info if you "
+            "double-click any of its text"
         )
+
+
+class EditorWithTripleClick(CellsTestPage):
+    def cell(self):
+        editor = cells.Editor(
+            onTripleClick=lambda *args: (
+                currentContents := editor.getCurrentContents(),
+                lines := currentContents.split("\n"),
+                editor.setContents(
+                    currentContents + "\n" + lines[min(args[0], len(lines) - 1)]
+                ),
+            )
+        )
+
+        return editor
+
+    def text(self):
+        return (
+            "You should see an editor which appends the highlighted line if you "
+            "triple-click any of its text"
+        )
+
+
+def test_editor_with_triple_click(headless_browser):
+    def triple_click(element, delay=0.1):
+        headless_browser.action.move_to_element(element).click().perform()
+        time.sleep(delay)
+        headless_browser.action.move_to_element(element).click().perform()
+        time.sleep(delay)
+        headless_browser.action.move_to_element(element).click().perform()
+
+    def type_letters(element, text, delay=0.1):
+        for c in text:
+            element.send_keys(c)
+            time.sleep(delay)
+
+    demo_root = headless_browser.get_demo_root_for(EditorWithTripleClick)
+    assert demo_root
+    assert demo_root.get_attribute("data-cell-type") == "Editor"
+    editor_line_layer = demo_root.find_element(
+        headless_browser.by.XPATH, ".//*[@class='editor-line-layer']"
+    )
+    assert editor_line_layer
+    editor_line_query = "//*[@class='editor-line']"
+    editor_lines = editor_line_layer.find_elements(
+        headless_browser.by.XPATH, editor_line_query
+    )
+    assert len(editor_lines) == 1
+    editor_selection_highlight_query = "//*[@class='editor-selection-highlight']"
+    editor_selection_highlights = demo_root.find_elements(
+        headless_browser.by.XPATH, editor_selection_highlight_query
+    )
+    assert len(editor_selection_highlights) == 1
+
+    test_text = (
+        "Line 1\n"
+        " Line 2 with a leading space\n"
+        "Line 3 with a trailing space\n"
+        " Line 4 with a leading and a trailing space \n"
+        "Line 5 which is the last line with text\n"
+    )
+
+    text_length = len(test_text.split("\n"))
+
+    def line_condition(browser, text_length):
+        return (
+            len(browser.find_elements(headless_browser.by.XPATH, editor_line_query))
+            >= text_length
+        )
+
+    type_letters(demo_root, test_text)
+
+    headless_browser.wait().until(lambda browser: line_condition(browser, text_length))
+    editor_lines = editor_line_layer.find_elements(
+        headless_browser.by.XPATH, editor_line_query
+    )
+    lines = [editor_line.text for editor_line in editor_lines]
+    assert len(lines) == text_length
+
+    line_index = 4
+    result = re.search(r"top:\s*(-?\d+)px", editor_lines[line_index].get_attribute("style"))
+    assert result
+    assert len(result.groups()) == 1
+    line_top = int(result.group(1))
+    triple_click(editor_lines[line_index])
+    text_length += 1
+    lines.append(str(text_length) + lines[line_index].replace("\n", "")[1:])
+
+    headless_browser.wait().until(lambda browser: line_condition(browser, text_length))
+    editor_lines = editor_line_layer.find_elements(
+        headless_browser.by.XPATH, editor_line_query
+    )
+    assert len(editor_lines) == len(lines)
+
+    for i, editor_line in enumerate(editor_lines):
+        assert editor_line.text == lines[i]
+
+    editor_selection_highlights = demo_root.find_elements(
+        headless_browser.by.XPATH, editor_selection_highlight_query
+    )
+    assert len(editor_selection_highlights) == 2
+    value = editor_selection_highlights[1].get_attribute("style")
+    result = re.search(r"left:\s*(-?\d+)px;\s*top:\s*(-?\d+)px", value)
+    assert result
+    assert len(result.groups()) == 2
+    selection_left = int(result.group(1))
+    selection_top = int(result.group(2))
+    assert selection_left == 50
+    assert abs(selection_top - line_top) < 5

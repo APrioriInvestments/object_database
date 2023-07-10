62ee9c304070e71ab97add2278714dbf55921a0c
dkrasner <krasnerdaniel@gmail.com>
2023-07-10 16:53:45 +0200

    updating table layout and related styling

--- object_database/web/devtools/js/cell_panel.js
+++ object_database/web/devtools/js/cell_panel.js
@@ -61,6 +61,8 @@ const clearDisplay = () => {
 // display the tree
 const cellsTreeDisplay = (cells) => {
     clearDisplay();
+    const info = document.getElementById("cell-info");
+    info.textContent = "click on a node to see more info";
     // init and run
     // NOTE: the tree class itself attaches the
     // svg element to #main
@@ -127,6 +129,11 @@ const updateInfoPanel = (node) => {
 
     infoSpan.innerText = info;
     infoPanel.append(infoSpan);
+    // args/props section
+    const span = document.createElement('span');
+    span.textContent = "Cell Arguments";
+    span.setAttribute("id", "node-args");
+    infoPanel.append(span);
 }
 
 
@@ -135,11 +142,13 @@ const createPropsTable = (propsDict) => {
     // header
     const thead = document.createElement('thead');
     const tr = document.createElement('tr');
-    const th = document.createElement('table');
+    const thName = document.createElement('th');
+    const thVal = document.createElement('th');
     thead.append(tr);
-    tr.append(th);
-    th.textContent = "Node Arguments";
-    th.setAttribute("colspan", 2);
+    tr.append(thName);
+    tr.append(thVal);
+    thName.textContent = "Name";
+    thVal.textContent = "Val";
     table.append(thead);
     // body
     const tbody = document.createElement('tbody');
@@ -147,9 +156,10 @@ const createPropsTable = (propsDict) => {
     Object.keys(propsDict).forEach((key) => {
         const tr = document.createElement('tr');
         const tdKey = document.createElement('td');
-        const tdValue = document.createElement('td');
+        const tdValue = document.createElement('input');
+        tdValue.setAttribute("type", "text");
         tdKey.textContent = key;
-        tdValue.textContent = propsDict[key];
+        tdValue.setAttribute("placeholder", propsDict[key]);
         tr.append(tdKey);
         tr.append(tdValue);
         tbody.append(tr);
